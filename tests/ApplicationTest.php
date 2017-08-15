2bb46027c8658ba0859b902b645f1c6af0038b08
mokamoto12 <mokamoto1208@gmail.com>
2017-08-15 23:13:13 +0900

    Add application test

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Mokamoto12\OopPractice;
 
+use Mokamoto12\OopPractice\Infrastructure\Persistence\Fixture\ProductRepositoryFixture;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -9,7 +10,34 @@ use PHPUnit\Framework\TestCase;
  */
 class ApplicationTest extends TestCase
 {
-    public function testMain()
+    /**
+     * @var Application
+     */
+    public $app;
+
+    public function setUp()
     {
+        $this->app = new Application(new ProductRepositoryFixture());
+    }
+
+    public function testRun()
+    {
+        ob_start();
+        $this->app->run([1 => 'A']);
+        $this->assertEquals("A: 1,000\nA: 1,005\n", ob_get_clean());
+    }
+
+    public function testRun2()
+    {
+        ob_start();
+        $this->app->run([1 => '--help']);
+        $this->assertEquals("Product name: --help is not found.\n", ob_get_clean());
+    }
+
+    public function testRun3()
+    {
+        ob_start();
+        $this->app->run([]);
+        $this->assertEquals("Please enter product name.\n", ob_get_clean());
     }
 }
